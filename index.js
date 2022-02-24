bcb0aa058eb5ce038b83711022f4177c847cdbd5
MarinaBotnareva <botmarinka@gmail.com>
2022-02-24 15:06:32 +0200

    Fix bug

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ let table = document.createElement('table');
 let thead = document.createElement('thead');
 let tbody = document.createElement('tbody');
 
-table.appendChild(thead);
-table.appendChild(tbody);
-body.appendChild(table)
+table.append(thead, tbody);
+body.append(table)
 
 let row_head = document.createElement('tr');
 let heading_1 = document.createElement('th');
@@ -13,9 +12,8 @@ heading_1.innerHTML = "Product";
 let heading_2 = document.createElement('th');
 heading_2.innerHTML = "Price";
 
-row_head.appendChild(heading_1);
-row_head.appendChild(heading_2);
-thead.appendChild(row_head);
+row_head.append(heading_1, heading_2);
+thead.append(row_head);
 
 const list = [
   { name: 'apple', price: 30 },
@@ -32,7 +30,6 @@ list.forEach((value) => {
   let data2 = document.createElement('td');
   data2.innerHTML = value.price;
 
-  row.appendChild(data1);
-  row.appendChild(data2);
-  tbody.appendChild(row);
+  row.append(data1, data2);
+  tbody.append(row);
 })
